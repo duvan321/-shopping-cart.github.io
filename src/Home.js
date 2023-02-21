7444d303b2365a0ff9c51b6da01738559d6ab87b
Diego patiño <diegoduvan321@gmail.com>
2023-02-21 18:57:42 -0500

    first commit

---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ItemCard from "./ItemCard";
+import data from "./data";
+function Home() {
+  return (
+    <>
+      <h1 className="text-center mt-3">All Items</h1>
+      <section className="py-4 container">
+        <div className="row justify-content-center">
+          {data.productData.map((item, index) => {
+            return (
+              <ItemCard
+                img={item.img}
+                title={item.title}
+                desc={item.desc}
+                price={item.price}
+                item={item}
+                key={index}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/data.js
@@ -0,0 +1,31 @@
+import img1 from "./img/grapes.png";
+import img2 from "./img/orange.png";
+import img3 from "./img/strawberries.png";
+
+const data = {
+  productData: [
+    {
+      id: 1,
+      img: img1,
+      title: "juicy Grapes",
+      desc: "",
+      price: "100",
+    },
+    {
+      id: 2,
+      img: img2,
+      title: "sweet oranges",
+      desc: "",
+      price: "150",
+    },
+    {
+      id: 3,
+      img: img3,
+      title: "juicy   Strawberries",
+      desc: "",
+      price: "200",
+    },
+  ],
+};
+
+export default data;
